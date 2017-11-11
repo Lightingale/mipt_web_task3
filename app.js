74daa1e28e6929ba76ca7a2e31ecb67f6b9240af
Lightingale <a.lightingale@gmail.com>
2017-11-12 02:48:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $("#root").append($("<ul id='list'></ul>"));
     $("#list").append($("<li id='element1'></li>"));
     $("#element1").append($("<span></span>").text("Сделать задание #3 по web-программированию"));
-    $("#element1").append('<p><button onclick="this.parentNode.parentNode.remove()">Удалить</button></p>');
+    $("#element1").append('<button onclick="this.parentNode.remove()">Удалить</button>');
 
     $("#root").append($("<p>Добавить задание: <input id='add_task_input'>  </input></p>"));
     $("#root").append($("<p><button id = 'add_task'>Добавить!</button></p>"));
@@ -15,7 +15,7 @@ $(document).ready(function () {
     $("#add_task").click(function(){
         $("#list").append($("<li id = " + tasks_num + "></li>"));
         $("#" + tasks_num).append($("<span></span>").text($("#add_task_input").val()));
-        $("#" + tasks_num).append('<p><button onclick="this.parentNode.parentNode.remove()">Удалить</button></p>');
+        $("#" + tasks_num).append('<button onclick="this.parentNode.remove()">Удалить</button>');
         tasks_num += 1;
     });
 
